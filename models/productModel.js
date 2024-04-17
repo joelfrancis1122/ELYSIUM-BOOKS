b1135ca80025ddbddb28eb0106e4bd7fe6442cc1
JoelFrancis1122 <joelfrancis9398@gmail.com>
2024-04-17 16:55:29 +0530

    xommit

--- models/productModel.js
+++ models/productModel.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 const productSchema=mongoose.Schema({
     Bookname:{type:String,required:true},
     Description:{type:String,required:true},
-    Categories:{type:ObjectId,ref:'Category',unique:true},
-    subCategories:{type:ObjectId,ref:'SubCategory',unique:true},
+    Categories:{type:ObjectId,ref:'Category'},
+    subCategories:{type:ObjectId,ref:'SubCategory'},
     Regularprice:{type:Number,required:true},
     saleprice:{type:Number,required:true},
     stock:{type:Number,required:true},
